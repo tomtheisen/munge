d0e50b160b810118070187be7d4071293e24ca97
Tom Theisen <github@tomtheisen.com>
2021-11-27 01:09:43 -0800

    just a lil ui polish

--- ui/ui.tsx
+++ ui/ui.tsx
@@ -12,14 +12,14 @@ class MungerApp extends RedactioComponent {
         super(
             <div>
                 <h2>Munging Code</h2>
-                <textarea ref="code" oninput={() => this.codeChanged()}></textarea>
+                <textarea ref="code" oninput={() => this.codeChanged()} spellcheck={false}></textarea>
                 <div ref="codeError" class="error" hidden></div>
-                <h2>Input Document</h2>
-                <textarea ref="input"></textarea>
-                <button onclick={() => this.munge()}>Munge me</button>
+                <h2>Input Document <button onclick={() => this.munge()}>▶ Munge</button></h2>
+                <textarea ref="input" spellcheck={false}></textarea>
+                
                 <div ref="outputPanel" hidden>
-                    <h2>Output</h2>
-                    <div ref="output"></div>
+                    <h2>Output <button onclick={() => this.copyOutput()}>⧉ Copy</button></h2>
+                    <div ref="output" id="output"></div>
                 </div>
             </div>);
 
@@ -28,6 +28,20 @@ class MungerApp extends RedactioComponent {
 
     get code() { return this.refs.code as HTMLTextAreaElement; }
     get input() { return this.refs.input as HTMLTextAreaElement; }
+    get outputPanel() { return this.refs.outputPanel as HTMLDivElement; }
+    get output() { return this.refs.output as HTMLDivElement; }
+
+    copyOutput() {
+        const range = document.createRange();
+        range.selectNodeContents(this.output);
+        let selection = getSelection();
+        if (selection) {
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+
+        navigator.clipboard.writeText(this.refs.output.innerText);
+    }
 
     saveState() {
         localStorage.setItem(MungerSourceKey, this.code.value);
