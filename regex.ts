951f0ad4234ce069a007455d31fc1fb0ea6ec02e
Tom Theisen <github@tomtheisen.com>
2021-12-05 19:37:37 -0800

    regex concept sketch

---
+++ regex.ts
@@ -0,0 +1,27 @@
+/*
+
+Reversable
+    \b
+    ^$
+    character classes https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes
+    |
+    []
+    [^]
+    (?:)
+    *
+    +
+    ?
+    {,}
+    \p{}
+    \u0000
+
+
+
+Not reversable
+    lookarounds
+    ()
+    \1
+    \k
+
+
+ */
